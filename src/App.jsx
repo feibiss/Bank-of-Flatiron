f7deb87dcfee2b14b54596757bca80eeed34c32c
feibiss <fabiansang@gmail.com>
2024-04-27 20:12:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import '.button.css';
+
+function App() {
+
+  return (
+    <>
+        <Button buttonType={''} buttontext ={'Normal'}/>
+        
+        
+        <Button buttonType={''} buttontext ={'Success'}/>
+
+    </>
+  )
+}
+
+export default App
